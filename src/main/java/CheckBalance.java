e978a0300f77933aef8cf44ac608fb056070152a
LogicWen <wkyworkuno@163.com>
2017-04-06 23:42:47 +0800

    first init reponsitory

---
+++ src/main/java/CheckBalance.java
@@ -0,0 +1,19 @@
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Andy on 2017/4/6.
+ *
+ * 判断一个树是否为完全二叉树
+ */
+public class CheckBalance {
+
+    public boolean chk(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        boolean flag = true;
+        boolean res = true;
+        return true;
+    }
+}
